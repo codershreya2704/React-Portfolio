66aaa47edd7441adf145585bb36f6b92de385a67
shreyatambad <52215220+codershreya2704@users.noreply.github.com>
2024-11-24 20:20:21 +0530

    Create constants.js

---
+++ Data/constants.js
@@ -0,0 +1,300 @@
+export const Bio = {
+    name: "Shreya Tambad",
+    roles: [
+      "Front End Developer",
+      "Android Developer",
+      "Java Developer",
+      "Programmer",
+    ],
+    description:
+      "I’m a fresh graduate passionate about web development and Java programming. I enjoy solving problems, learning new technologies, and turning ideas into functional, creative projects. My goal is to keep improving and contribute to meaningful work in tech.",
+    github: "https://github.com/codershreya2704",
+    resume:
+      "https://drive.google.com/file/d/1PIlF34c67TjybirVCaLa7yLiqQVmlKQ8/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/shreyatambad/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        
+        {
+          name: "XML",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+        },
+        {
+          name: "Android Studio",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        
+        
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "",
+      role: "Software Intern",
+      company: "JANGS Technologies",
+      date: " July 2023 - Aug 2023",
+      desc: "Working with Meta Verse Technology, managing payments of an e-commerce website, and managing team.",
+      skills: [
+        "C#",
+        "HTML5",
+        "CSS",
+        "Firebase",
+        "Razorpay",
+        "API",
+        "JavaScript",
+      ],
+      doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+    {
+      id: 0,
+      img: "",
+      role: "Frontend Engineer Intern",
+      company: "Internshala",
+      date: "June 2021 - July 2021",
+      desc: "Working on the frontend of the web application using HTML, CSS, Bootstrap, Javascript.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Bootstrap",
+        "JavaScript",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    },
+   
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "",
+      school: "Dr. J. J. Magdum College of Engineering, Jaysingpur",
+      date: "Oct 2021 - Sep 2024",
+      grade: "7.4 CGPA",
+      desc: "I recently graduated with a Bachelor's degree in Computer Science and Engineering at Dr. J. J. Magdum College of Engineering, Jaysingpur. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "",
+      school: "Sharad Institute of Technology and Polytechnic, Yadrav.",
+      date: "Apr 2019 - Apr 2024",
+      grade: "87.6%",
+      desc: "I completed diploma at Sharad Institute of Technology and Polytechnic, Yadrav, where I studied Science with Computer Science.",
+      degree: "Diploma, Science with Computer",
+    },
+    {
+      id: 2,
+      img: "",
+      school: "Balvantrao Zele Highschool,Jaysingpur",
+      date: "Apr 2016 - Apr 2017",
+      grade: "72.3%",
+      desc: "I completed my class 10 education at Balvantrao Zele Highschool,Jaysingpur.",
+      degree: "MSBHSC(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Training and Placement Website",
+      date: "Oct 2022 - March 2023",
+      description:
+        "Developed a user-friendly Training and Placement for our college to keep track of students placement and their interview status including the practice portal where students can access practice material. ",
+      image:
+        "https://github.com/codershreya2704/T-P-cell-/raw/a4617895d84e5ff5ce048ebb8d95566c55643de8/Asset/Tnp.png",
+      tags: [
+        "MySql",
+        "Javascript",
+        "PHP",
+        "HTML5",
+        "CSS3",
+        "Bootstrap",
+        
+      ],
+      category: "web app",
+      github: "https://github.com/codershreya2704/T-P-cell-",
+      
+    },
+    {
+      id: 0,
+      title: "Student Smart Card System",
+      date: "Oct 2019 - Apr 2021",
+      description:
+        "Developed a centralized system for students where they can make their day-to-day payments using a dedicated student smart card containing barcode that students can scan and make payments. ",
+      image:
+        "https://github.com/codershreya2704/T-P-cell-/raw/a4617895d84e5ff5ce048ebb8d95566c55643de8/Asset/smart card.png",
+      tags: ["HTML5", "CSS3", "Javascript", "code128"],
+      category: "web app",
+      
+    },
+    {
+      id: 1,
+      title: "Social Media Using Blockchain",
+      date: "Oct 2023 - March 2024",
+      description:
+        "Developed a social media app with an integrated blockchain technology for seamless payments and security. ",
+      image:
+        "https://github.com/codershreya2704/T-P-cell-/raw/a4617895d84e5ff5ce048ebb8d95566c55643de8/Asset/blockchain.png",
+      tags: [
+        "Java",
+        "Firebase",
+        "XML",
+        "Android",
+
+      ],
+      category: "android app",
+    },
+    {
+      id: 1,
+      title: "Meta Verse",
+      date: "July 2023 - August 2023",
+      description:
+        "Worked on Meta Verse Technology, a digital universe that combines physical and digital reality, where users interact as avatars in a virtual world. ",
+      image:
+        "https://github.com/codershreya2704/T-P-cell-/raw/d7aed1ef62797123a996ee06a1b6f4441bf37e67/Asset/jangs.png",
+      tags: [
+        "Java",
+        "Firebase",
+        "XML",
+        "Android",
+
+      ],
+      category: "simulation",
+    },
+    {
+      id: 1,
+      title: "Snake Game",
+      date: "July 2024 - August 2024",
+      description:
+        "A standard snake game that everyone enjoys. ",
+      image:
+        "https://github.com/codershreya2704/SnakeGame_java/raw/b5b258abcce1300919fce88f6ae2993637b35274/Snake.png",
+      tags: [
+        "Java",
+        "Firebase",
+        "XML",
+        "Android",
+
+      ],
+      category: "web app",
+    },
+    {
+      id: 1,
+      title: "Calify",
+      date: "July 2023 - August 2023",
+      description:
+        "Working on C# based software to make Calibration Certificate and calculate uncertainity based on calculations. ",
+      image:
+        "https://github.com/codershreya2704/SnakeGame_java/raw/9f65d2314290589987e0d4dbffe2d6e48f970d4d/csharp.png",
+      tags: [
+        "C#",
+        ".Net",
+        "MySQL",
+
+      ],
+      category: "web app",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
